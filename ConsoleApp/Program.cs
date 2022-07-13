3bf7e9e631e0b945da609c800478be5f8230ad6d
Abise <Abise@DESKTOP-6088VQ0>
2022-07-12 21:05:26 -0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace ConsoleApp
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student(1,"Abisek","Kharel",25,"male",'B');
+            PrintObjectProperties(student);
+            
+        }
+        public static void PrintObjectProperties(Student obj)
+        {
+            Type t = typeof(Student);
+            var props = t.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var prop in props)
+            {
+                Console.WriteLine(prop.Name + " -  " + prop.GetValue(obj, null)+ " " + "DataType "+ " - " + prop.PropertyType);    
+            }           
+        }
+    }
+}
---
+++ ConsoleApp/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Student
+    {
+        private int Id { get; set; }
+     
+        private  string FirstName { get; set; }
+        private string LastName { get; set; }
+        private int Age { get; set; }
+        private string Gender { get; set; }
+        private Char Grade { get; set; }
+        public Student(int Id,string FirstName,string LastName,int Age, string Gender, Char Grade)
+        {
+            this.Id = Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+            this.Gender = Gender;
+            this.Grade = Grade;
+        }
+    }
+}
